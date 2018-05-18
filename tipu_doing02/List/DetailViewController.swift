4b5be613e4f26c2e1a4136a184850932243f0666
sweepty <adie0423@gmail.com>
2018-05-19 03:28:26 +0900

    [추가] snapkit 적용

--- tipu_doing02/List/DetailViewController.swift
+++ tipu_doing02/List/DetailViewController.swift
@@ -29,14 +29,13 @@ class DetailViewController: UIViewController {
     @IBOutlet var accountholderlabel: UILabel!
     @IBOutlet var moneylabel: UILabel!
     
+    @IBOutlet var infoView: UIView!
     @IBOutlet var ticketimage: UIImageView!
     
     @IBOutlet var text1: UILabel!
     @IBOutlet var text2_account: UILabel!
     @IBOutlet var text3_accountholder: UILabel!
     @IBOutlet var text4_money: UILabel!
-    
-    
     
     @IBOutlet var depositbutton: UIButton!
     
@@ -46,7 +45,53 @@ class DetailViewController: UIViewController {
         let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressLabel(longPressGestureRecognizer:)))
         accountlabel.addGestureRecognizer(longPressGestureRecognizer)
         accountlabel.isUserInteractionEnabled = true
+        
+        //snapkit
+        let superView = self.view
+        //티켓 이미지
+        self.ticketimage.snp.makeConstraints { (make) -> Void in
+            make.width.height.equalTo(67)
+            make.top.equalTo(30)
+            make.left.equalTo(30)
+        }
+        // 티켓 제목 길이가 길어지면 넘어가는 문제;-; 우선 넘어가자  폰트를 키워야할듯
+        self.titlelabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(120)
+            make.height.equalTo(30)
+            make.left.right.equalTo(25)
+            
+        }
+        // 입금 기한
+        self.deadlinelabel.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(160)
+            make.height.equalTo(25)
+            make.left.right.equalTo(35)
+        }
+        //메세지
+        self.text1.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(195)
+            make.height.equalTo(10)
+            make.left.right.equalTo(35)
+        }
+        //아래 상세 뷰
+        self.infoView.snp.makeConstraints { (make) -> Void in
+            make.top.greaterThanOrEqualTo(250)
+            make.top.lessThanOrEqualTo(290)
+            make.width.lessThanOrEqualTo((superView?.layoutMarginsGuide)!)
+            make.center.equalTo((superView?.layoutMarginsGuide)!)
+            make.left.equalTo((superView?.layoutMarginsGuide)!)
+        }
+
+        //입금상태 변경 버튼
+        self.depositbutton.snp.makeConstraints { (make) -> Void in
+            make.right.left.equalTo(superView!).offset(0)
+            make.height.equalTo(60)
+            make.bottom.equalTo(0)
+        }
+        
+        
     }
+    
     @objc private func longPressLabel (longPressGestureRecognizer: UILongPressGestureRecognizer) {
         if longPressGestureRecognizer.state == .began {
             let copy_accountNumber = accountlabel.text
@@ -77,7 +122,7 @@ class DetailViewController: UIViewController {
             
             if  deposit == true {
                 // 입금 완료
-                ticketimage.image = UIImage(named:"pink_ticket")
+                ticketimage.image = UIImage(named:"detail_paid")
                 self.view.backgroundColor = UIColor.black
                 depositbutton.backgroundColor = UIColor.white
                 depositbutton.setTitleColor(UIColor.black, for: UIControlState.normal)
@@ -97,7 +142,7 @@ class DetailViewController: UIViewController {
             }
             else {
                 // 미입금
-                ticketimage.image = UIImage(named:"white_ticket")
+                ticketimage.image = UIImage(named:"detail_not_paid")
                 self.view.backgroundColor = UIColor.white
                 depositbutton.backgroundColor = UIColor(red:1, green:82/255, blue:140/255, alpha:1)
                 depositbutton.setTitleColor(UIColor.white, for: UIControlState.normal)
@@ -138,7 +183,7 @@ class DetailViewController: UIViewController {
                 moneylabel.textColor = UIColor.white
                 
                 text1.textColor = UIColor.white
-                text1.text = "입금 완료됨"
+                text1.text = "입금이 완료되었습니다"
                 text2_account.textColor = UIColor.white
                 text3_accountholder.textColor = UIColor.white
                 text4_money.textColor = UIColor.white
